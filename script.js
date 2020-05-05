bfde0bb72016279b9c853adc2218a7521f25386a
cethread <43501032+cethread@users.noreply.github.com>
2020-05-04 20:26:23 -0400

    upd

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 const getStartedBtn = document.querySelector(".sign-in .btn-slide-down");
 const signInBtn = document.querySelector(".btn-slide-left");
 const signInSection = document.querySelector(".sign-in")
-const logo = document.querySelector(".logo");
+const icon = document.querySelector(".icon");
+const logoImage = document.querySelector(".logo a");
 const hamburger = document.querySelector(".hamburger");
 const username = document.querySelector(".username");
 const password = document.querySelector(".password");
@@ -11,7 +12,7 @@ const forgotPassword = document.querySelector(".forgot-password");
 
 function signIn () {
     signInSection.classList.toggle("sign-in-trigger");
-    logo.classList.toggle("sign-in-trigger");
+    icon.classList.toggle("sign-in-trigger");
     hamburger.classList.toggle("menu-btn-trigger");
     password.classList.toggle("menu-btn-trigger");
     signInBtn.classList.toggle("sign-in-trigger");
@@ -19,5 +20,9 @@ function signIn () {
     
 }
 
+function lowOpacity () {
+    icon.style.opacity = "0.5";
+}
 getStartedBtn.addEventListener("click", signIn, false);
+logoImage.addEventListener("mouseover" , lowOpacity, false)
 console.log("working")
